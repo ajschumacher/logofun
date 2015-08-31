4b19cc94f460a4f3632f863fe2ef96afc34b35c3
Aaron Schumacher <ajschumacher@gmail.com>
2015-08-30 21:30:17 -0400

    throw in function

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ var hexSide = width / 1.5;
 var height = hexSide * Math.sqrt(3) / 2;
 
 var radius = hexSide/6;  // radius of "cell body"
-var clear = hexSide/12;  // width of "synapse"
-var delta = 0.4;  // radian half-width at circle
+var clear = hexSide/8;  // width of "synapse"
+var delta = Math.PI / 6;  // radian half-width at circle
 
 var mainColor = "#000066";
 var background = "#FFFFFF";
@@ -24,50 +24,50 @@ var svg = d3.select("div").append("svg")
         .attr("width", width)
         .attr("height", height);
 
-svg.append("rect")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("fill", background);
+var drawLogo = function(selection) {
+    var origin = selection.append("g")
+            .translate([width/2, height/2]);
 
-var origin = svg.append("g").translate([width/2, height/2]);
+    triangle = function(selection, first, second, third) {
+        var triangle = selection.append("path")
+                .attr("d", "M " + first[0] + " " + first[1]
+                      + " L " + second[0] + " " + second[1]
+                      + " L " + third[0] + " " + third[1]);
+        return triangle;
+    };
 
-triangle = function(selection, first, second, third) {
-    var triangle = selection.append("path")
-        .attr("d", "M " + first[0] + " " + first[1]
-              + " L " + second[0] + " " + second[1]
-              + " L " + third[0] + " " + third[1]);
-    return triangle;
+    mirror = function(first, second, third, f) {
+        f(first, second, third);
+        f([-first[0], first[1]],
+          [-second[0], second[1]],
+          [-third[0], third[1]]);
+        f([first[0], -first[1]],
+          [second[0], -second[1]],
+          [third[0], -third[1]]);
+        f([-first[0], -first[1]],
+          [-second[0], -second[1]],
+          [-third[0], -third[1]]);
+    };
+
+    var theta = Math.atan((height/2) / (width/2));
+    var x1 = Math.cos(theta - delta) * radius;
+    var y1 = Math.sin(theta - delta) * radius;
+    var x2 = Math.cos(theta + delta) * radius;
+    var y2 = Math.sin(theta + delta) * radius;
+
+    mirror([x1, y1], [width/2, height/2], [x2, y2],
+           function(first, second, third) {
+               triangle(origin, first, second, third)
+                   .attr("fill", mainColor);
+           });
+
+    origin.append("circle")
+        .attr("r", radius + clear)
+        .attr("fill", background);
+
+    origin.append("circle")
+        .attr("r", radius)
+        .attr("fill", mainColor);
 };
 
-mirror = function(first, second, third, f) {
-    f(first, second, third);
-    f([-first[0], first[1]],
-      [-second[0], second[1]],
-      [-third[0], third[1]]);
-    f([first[0], -first[1]],
-      [second[0], -second[1]],
-      [third[0], -third[1]]);
-    f([-first[0], -first[1]],
-      [-second[0], -second[1]],
-      [-third[0], -third[1]]);
-};
-
-var theta = Math.atan((height/2) / (width/2));
-var x1 = Math.cos(theta - delta) * radius;
-var y1 = Math.sin(theta - delta) * radius;
-var x2 = Math.cos(theta + delta) * radius;
-var y2 = Math.sin(theta + delta) * radius;
-
-mirror([x1, y1], [width/2, height/2], [x2, y2],
-       function(first, second, third) {
-           triangle(origin, first, second, third)
-               .attr("fill", mainColor);
-       });
-
-origin.append("circle")
-    .attr("r", radius + clear)
-    .attr("fill", background);
-
-origin.append("circle")
-    .attr("r", radius)
-    .attr("fill", mainColor);
+drawLogo(svg);
